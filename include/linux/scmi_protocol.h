45fd19d6b26245d1e9e3973d280c70ccba02b2b4
Cristian Marussi <cristian.marussi@arm.com>
2023-03-15 09:32:27 +0000

    firmware: arm_scmi: Add common per-protocol debugfs support
    Allow interested SCMI protocols to register their own specific debugfs
    entries under a common per-instance and per-protocol subtree rooted
    at /sys/kernel/debug/scmi/<N>/protocols/<PROTO_ID>/
    
    Expose a helper to enable protocol initialization code to get access to
    such per-protocol/per-instance dentries in order to be able to install
    their own dedicated debugfs entries.
    
    Per-protocol debugfs support is configurable and default off.
    
    Signed-off-by: Cristian Marussi <cristian.marussi@arm.com>
    ---
    v5 --> v6
     - bail out of dentry_get helper when SCMI debug susbsytem failed to init
     - remove stale SCMI protocol operations
     - add a preoper clean up of debugfs when protocol init fails

--- include/linux/scmi_protocol.h
+++ include/linux/scmi_protocol.h
@@ -1141,7 +1141,6 @@ struct scmi_handle {
 	void (*devm_protocol_put)(struct scmi_device *sdev, u8 proto);
 	bool (*is_transport_atomic)(const struct scmi_handle *handle,
 				    unsigned int *atomic_threshold);
-
 	const struct scmi_notify_ops *notify_ops;
 };
 
