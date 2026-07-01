b260b4908fe72038e99f74242d30867ecbe3e313
Cristian Marussi <cristian.marussi@arm.com>
2026-07-01 18:12:45 +0100

    firmware: arm_scmi: Expose per-instance identifier
    When multiple SCMI instances are configured, the SCMI stack probes and
    it is initialized multiple times: one core stack for each defined instance.
    
    Each istance is simply identified internally with a number and such number
    is already exposed to the user to name the root of the per-instance debugfs
    subtree, if enabled.
    
    Expose such unique identifier to the SCMI drivers so that they can use
    that same identifier when in need to name their per-instance resources.
    
    Signed-off-by: Cristian Marussi <cristian.marussi@arm.com>
    ---
    v7 --> v8
     - fixed Doxygen 'non-negative' integer

--- include/linux/scmi_protocol.h
+++ include/linux/scmi_protocol.h
@@ -1125,6 +1125,10 @@ struct scmi_notify_ops {
 /**
  * struct scmi_handle - Handle returned to ARM SCMI clients for usage.
  *
+ * @id: A unique non-negative natural integer identifying the SCMI Instance
+ *	associated with this handle to be used across all drivers for
+ *	naming purposes: same identifier used internally as the root for
+ *	the debugfs per-instance tree.
  * @dev: pointer to the SCMI device
  * @version: pointer to the structure containing SCMI version information
  * @devm_protocol_acquire: devres managed method to get hold of a protocol,
@@ -1145,6 +1149,7 @@ struct scmi_notify_ops {
  * @notify_ops: pointer to set of notifications related operations
  */
 struct scmi_handle {
+	unsigned int id;
 	struct device *dev;
 	struct scmi_base_info *version;
 
